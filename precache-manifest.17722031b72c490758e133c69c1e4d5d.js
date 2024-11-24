4059698930eecdc726a26cb09cf83d1259f94d02
Zene Ansari <zeneansari@Zenes-MacBook-Air.local>
2024-11-24 19:50:35 +0530

    Updates

---
+++ precache-manifest.17722031b72c490758e133c69c1e4d5d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbfb50b42721a015d1edf6fa3a4d6d7b",
+    "url": "/react-ecomm/index.html"
+  },
+  {
+    "revision": "912603724f2b6fb7ebb7",
+    "url": "/react-ecomm/static/css/main.1402fc47.chunk.css"
+  },
+  {
+    "revision": "912603724f2b6fb7ebb7",
+    "url": "/react-ecomm/static/js/main.907a8f9b.chunk.js"
+  },
+  {
+    "revision": "65a599e36f7845b19730",
+    "url": "/react-ecomm/static/js/runtime~main.47b52ead.js"
+  },
+  {
+    "revision": "2412edbf01fd654c8743ceaf8dda9721",
+    "url": "/react-ecomm/static/media/1.2412edbf.png"
+  },
+  {
+    "revision": "95d6d8bc0daba20afaa7a392312df717",
+    "url": "/react-ecomm/static/media/2.95d6d8bc.png"
+  },
+  {
+    "revision": "228eb0826845a795979dde82251361cc",
+    "url": "/react-ecomm/static/media/3.228eb082.png"
+  },
+  {
+    "revision": "3a7169d75c9cb9bd0927f87ddcb4a2e6",
+    "url": "/react-ecomm/static/media/4.3a7169d7.png"
+  },
+  {
+    "revision": "ad380cb478bb255d81f99e612b36c95b",
+    "url": "/react-ecomm/static/media/5.ad380cb4.png"
+  },
+  {
+    "revision": "b33748074712b6e8bff43cff15ab27e4",
+    "url": "/react-ecomm/static/media/6.b3374807.png"
+  },
+  {
+    "revision": "1bd63042cdbb68d4fd4e3891e3d717d4",
+    "url": "/react-ecomm/static/media/bg.1bd63042.jpeg"
+  }
+]);
